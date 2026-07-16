b14d17739cb670678a18869f755191d094385065
HoorHasan30 <202301820@student.polytechnic.bh>
2026-07-16 10:10:59 +0300

    Feat: Add index controller routes

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,3 +1,29 @@
 const router = require('express').Router()
 
+// Home Page 
+// GET:
+router.get('/home', 
+    (req, res) => {
+       try{
+        res.render('index.ejs')
+       }
+       catch(err){
+        console.log(err)
+       } 
+    }
+)
+
+// About Page
+// GET:
+router.get('/about', 
+    (req, res) => {
+        try{
+            res.render('about.ejs')
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+)
+
 module.exports = router;
