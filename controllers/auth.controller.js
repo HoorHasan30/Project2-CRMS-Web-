4472b5cee619cb3420a0927af0e497d1c29d2f17
HoorHasan30 <202301820@student.polytechnic.bh>
2026-07-16 10:37:14 +0300

    Feat: Add auth routes

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,122 @@
 const router = require('express').Router()
+const User = require("../models/user.js");
+const bcrypt = require("bcrypt");
+
+// Sign Up
+// GET:
+router.get('/sign-up', 
+    (req, res) => {
+        try{
+            res.render('auth/sign-up.ejs')
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+)
+
+// POST:
+router.post('/sign-up',
+    async (req, res) => {
+        try{
+            // check if username is used 
+            const userIsInDb = await User.findOne({ username: req.body.username })
+            if(userIsInDb) {
+                return res.send("Username already taken.");
+            }
+
+            // chek password fields match
+            if (req.body.password !== req.body.confirmPassword) {
+                return res.send("Password and Confirm Password must match");
+            }
+
+            // hash the pass
+            const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+            const username = req.body.username
+            // Create User
+            await User.create({
+                username,
+                password: hashedPassword
+            })
+
+            const getUser = await User.findOne({ username })
+
+            // login the user directly after login 
+            // set user Session
+            req.session.user = {
+                username,
+                _id: getUser._id
+            };
+
+            // redirect to home page 
+            res.redirect('/home')
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+)
+
+// Login
+// GET: 
+router.get('/login', 
+    (req, res) => {
+        try{
+            res.render('auth/login.ejs')
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+)
+
+// POST:
+router.post('/login',
+    async (req, res) => {
+        try{
+            // get user from db
+            const userIsInDb = await User.findOne({ username: req.body.username })
+            if(!userIsInDb) {
+                return res.send("Login failed. Please try again");
+            }
+
+            // validate password
+            const isValid = bcrypt.compareSync(
+                req.body.password,
+                userIsInDb.password
+            )
+            if (!isValid) {
+                return res.send("Login failed. Please try again.");
+            }
+
+            // set user Session
+            req.session.user = {
+                username: userIsInDb.username,
+                _id: userIsInDb._id
+            }
+
+            // redirect to home page 
+            res.redirect('/home')
+
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+)
+
+// Logout
+// GET:
+router.get('/sign-out', 
+    (req, res) => {
+        try{
+            req.session.destroy()
+            res.redirect('/home')
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+)
 
 module.exports = router;
