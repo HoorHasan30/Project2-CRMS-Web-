741d5741b5df503c2375c1a9167585bedb9ed7bb
HoorHasan30 <202301820@student.polytechnic.bh>
2026-07-19 13:41:38 +0300

    Style: Add style to Auth Pages

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -13,17 +13,5 @@ router.get('/home',
     }
 )
 
-// About Page
-// GET:
-router.get('/about', 
-    (req, res) => {
-        try{
-            res.render('about.ejs')
-        }
-        catch(err){
-            console.log(err)
-        }
-    }
-)
 
 module.exports = router;
