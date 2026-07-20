d2d6bc63a81961ccab271294985d036654c878d9
HoorHasan30 <202301820@student.polytechnic.bh>
2026-07-20 18:10:53 +0300

    Feat: Add Dashboard(Analysis) Routes

--- controllers/analysis.controller.js
+++ controllers/analysis.controller.js
@@ -1,8 +1,43 @@
 const router = require('express').Router()
 
+const Category = require('../models/Category')
+const Technicain = require('../models/Technician')
+const Ticket = require('../models/Ticket')
+
 router.get('/', 
-    (req, res) => {
-        res.render('analysis/analysis.ejs')
+    async (req, res) => {
+
+        // total numbers
+        const tickets = await Ticket.find()
+        const  totalTickets = await tickets.length
+
+        const categories = await Category.find()
+        const  totalCategories = await categories.length
+
+        const techs = await Technicain.find()
+        const  totalTechs = await techs.length
+
+
+        // Charts Data
+        const statusLabels = ['Pending', 'Assigned', 'Completed']
+        const statusCounts = [
+            tickets.filter(t => t.status === 'Pending').length,
+            tickets.filter(t => t.status === 'Assigned').length,
+            tickets.filter(t => t.status === 'Completed').length
+        ]
+
+
+        const prioratyLabels = ['High', 'Moderate', 'Low']
+        const prioratyCounts = [
+            tickets.filter(t => t.prioraty === 'High').length,
+            tickets.filter(t => t.prioraty === 'Moderate').length,
+            tickets.filter(t => t.prioraty === 'Low').length
+        ]
+
+        res.render('analysis/analysis.ejs', {totalTickets, totalCategories, totalTechs, 
+                                            statusLabels, statusCounts,
+                                            prioratyLabels, prioratyCounts
+                                            })
     }
 )
 
